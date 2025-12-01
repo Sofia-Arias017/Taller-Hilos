30d2c27a4f87466d25c33ecaaf05c8c0268a5043
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-01 16:53:19 -0500

    feat: :sparkles: Implementar clase Main que inicializa los equipos y ejecuta la carrera de relevos

--- relevos/src/main/java/com/relevos/Main.java
+++ relevos/src/main/java/com/relevos/Main.java
@@ -1,7 +1,14 @@
 package com.relevos;
 
+import com.relevos.dominio.Equipo;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Equipo equipoRojo = new Equipo("ROJO");
+        Equipo equipoAzul = new Equipo("AZUL");
+
+        equipoAzul.iniciarCarrera();
+        equipoRojo.iniciarCarrera();
     }
 }
