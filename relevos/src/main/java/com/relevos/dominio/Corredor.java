743082947ef7030f669a9a4b277f77bda5152763
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-01 15:47:53 -0500

    feat: :sparkles: Añade clase Corredor que usa hilos para simular espera, carrera y entrega de relevo

--- relevos/src/main/java/com/relevos/dominio/Corredor.java
+++ relevos/src/main/java/com/relevos/dominio/Corredor.java
@@ -1,5 +1,44 @@
 package com.relevos.dominio;
 
-public class Corredor {
+import java.util.Random;
+
+public class Corredor extends Thread {
+
+    private String nombre;
+    private Corredor anterior; 
     
+    public Corredor(String nombre, Corredor anterior) {
+        this.nombre = nombre;
+        this.anterior = anterior;
+    }
+
+    @Override
+    public void run() {
+        try {
+            
+            if (anterior != null) {
+                System.out.println(nombre + " esta esperando a que " + anterior.getNombre() + " termine para recibir el relevo.");
+                anterior.join();
+            } else {
+                System.out.println(nombre + " inicia la carrera.");
+            }
+
+            
+            System.out.println(nombre + " ha comenzado su recorrido.");
+
+            int tiempo = new Random().nextInt(2000) + 1000; 
+            Thread.sleep(tiempo);
+
+          
+            System.out.println(nombre + " ha terminado su recorrido y entrega el relevo.");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
+
