776bc321779fe8ce2ef6b1730291d96b18f3c418
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-01 16:46:57 -0500

    feat: :sparkles: Agregar clase Equipo para gestionar corredores e iniciar la carrera

--- relevos/src/main/java/com/relevos/dominio/Equipo.java
+++ relevos/src/main/java/com/relevos/dominio/Equipo.java
@@ -1,5 +1,30 @@
 package com.relevos.dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Equipo {
-    
+
+    private String nombreEquipo;
+    private List<Corredor> corredores = new ArrayList<>();
+
+    public Equipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+
+        Corredor c1 = new Corredor(nombreEquipo + "-1", null);
+        Corredor c2 = new Corredor(nombreEquipo + "-2", c1);
+        Corredor c3 = new Corredor(nombreEquipo + "-3", c2);
+
+        corredores.add(c1);
+        corredores.add(c2);
+        corredores.add(c3);
+    }
+
+    public void iniciarCarrera() {
+        System.out.println("\n=== Iniciando equipo " + nombreEquipo + " ===");
+
+        for (Corredor c : corredores) {
+            c.start();
+        }
+    }
 }
